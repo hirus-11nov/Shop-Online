435c5bbbd1cd4832ca7edf7b8c11cf79de0d5607
Hirus <hirusmeo@gmail.com>
2021-03-13 22:54:21 +0700

    sub

--- resources/views/frontend/pages/contact.blade.php
+++ resources/views/frontend/pages/contact.blade.php
@@ -138,7 +138,7 @@
 		<div class="modal-dialog" role="document">
 		  <div class="modal-content">
 			<div class="modal-header">
-				<h2 class="text-warning">Sorry!</h2>
+				<h2 class="text-warning">Xin lỗi!</h2>
 				<button type="button" class="close" data-dismiss="modal" aria-label="Close">
 					<span aria-hidden="true">&times;</span>
 				</button>
